823e801a5e076e3a2cee70005f5a968eb4ca8d13
Aurelien Deharbe <ad0@users.noreply.github.com>
2017-07-07 14:13:41 +0200

    changed some colors

--- xmonad.hs
+++ xmonad.hs
@@ -24,13 +24,13 @@ import System.IO
 import System.Exit
 
 
-myTerminal = "uxterm"
+myTerminal = "urxvt"
 myModMask = mod4Mask
 
 myWorkspaces = map show [1..6]
 
-myXmonadBar = "dzen2 -x '0' -y '0' -h '24' -w '800' -ta 'l' -fg '#FFFFFF' -bg '#1B1D1E'"
-myStatusBar = "conky -c /home/ad/.xmonad/conky_dzen | dzen2 -x '800' -y '0' -w '568' -h '24' -ta 'r' -fg '#FFFFFF' -bg '#1B1D1E'"
+myXmonadBar = "dzen2 -x '0' -y '0' -h '24' -w '840' -ta 'l' -fg '#FFFFFF' -bg '#5c5c5c'"
+myStatusBar = "conky -c /home/ad/.xmonad/conky_dzen | dzen2 -x '840' -y '0' -w '840' -h '24' -ta 'r' -fg '#FFFFFF' -bg '#5c5c5c'"
 myBitmapsDirs = "/home/ad/.xmonad/bitmaps_dzen"
 
 main = do
@@ -57,14 +57,14 @@ customLayout = avoidStruts $
 
 myLogHook :: Handle -> X ()
 myLogHook h = dynamicLogWithPP $ defaultPP
-  { ppCurrent         = dzenColor "#ebac54" "#1B1D1E" . pad
-  , ppVisible         = dzenColor "white" "#1B1D1E" . pad
-  , ppHidden          = dzenColor "white" "#1B1D1E" . pad
-  , ppHiddenNoWindows = dzenColor "#7b7b7b" "#1B1D1E" . pad
-  , ppUrgent          = dzenColor "#ff0000" "#1B1D1E" . pad
+  { ppCurrent         = dzenColor "#1e90ff" "#5c5c5c" . pad
+  , ppVisible         = dzenColor "#000000" "#5c5c5c" . pad
+  , ppHidden          = dzenColor "#000000" "#5c5c5c" . pad
+  , ppHiddenNoWindows = dzenColor "#7b7b7b" "#5c5c5c" . pad
+  , ppUrgent          = dzenColor "#ff0000" "#5c5c5c" . pad
   , ppWsSep           = " "
   , ppSep             = "  |  "
-  , ppLayout          = dzenColor "#edac54" "#1B1D1E" .
+  , ppLayout          = dzenColor "#1e90ff" "#5c5c5c" .
     (\x -> case x of
       "ResizableTall"        -> "^i(" ++ myBitmapsDirs ++ "/tall.xbm)"
       "Mirror ResizableTall" -> "^i(" ++ myBitmapsDirs ++ "/mtall.xbm)"
@@ -76,22 +76,22 @@ myLogHook h = dynamicLogWithPP $ defaultPP
   , ppOutput          = hPutStrLn h
   }
 
-myNormalBorderColor = "#1B1D1E"
-myFocusedBorderColor = "#00A880"
+myNormalBorderColor = "#5c5c5c"
+myFocusedBorderColor = "#1e90ff"
 myBorderWidth = 2
 
 myKeys =
-  [ ((mod4Mask,                       xK_p             ), runOrRaisePrompt defaultXPConfig)
-  , ((mod4Mask,                       xK_o             ), spawn "chromium")
-  , ((mod4Mask .|. shiftMask,         xK_z             ), spawn "xscreensaver-command -lock")
-  , ((mod4Mask,                       xK_b             ), sendMessage ToggleStruts)
-  , ((mod4Mask,                       xK_F1            ), spawn "xdotool key XF86AudioPrev")
-  , ((mod4Mask,                       xK_F2            ), spawn "xdotool key XF86AudioNext")
-  , ((mod4Mask,                       xK_F3            ), spawn "xdotool key XF86AudioPlay")
-  , ((mod4Mask,                       xK_F6            ), spawn "xbacklight -dec 10")
-  , ((mod4Mask,                       xK_F7            ), spawn "xbacklight -inc 10")
-  , ((mod4Mask,                       xK_F8            ), spawn "amixer sset Master toggle")
-  , ((mod4Mask,                       xK_F9            ), spawn "amixer -c 1 sset Master 2%-")
-  , ((mod4Mask,                       xK_F10           ), spawn "amixer -c 1 sset Master 2%+")
+  [ ((mod4Mask,                xK_p             ), runOrRaisePrompt defaultXPConfig)
+  , ((mod4Mask,                xK_o             ), spawn "firefox")
+  , ((mod4Mask .|. shiftMask,  xK_z             ), spawn "xscreensaver-command -lock")
+  , ((mod4Mask,                xK_b             ), sendMessage ToggleStruts)
+  , ((mod4Mask,                xK_F1            ), spawn "xdotool key XF86AudioPrev")
+  , ((mod4Mask,                xK_F2            ), spawn "xdotool key XF86AudioNext")
+  , ((mod4Mask,                xK_F3            ), spawn "xdotool key XF86AudioPlay")
+  , ((mod4Mask,                xK_F6            ), spawn "xbacklight -dec 10")
+  , ((mod4Mask,                xK_F7            ), spawn "xbacklight -inc 10")
+  , ((mod4Mask,                xK_F8            ), spawn "amixer sset Master toggle")
+  , ((mod4Mask,                xK_F9            ), spawn "amixer -c 1 sset Master 2%-")
+  , ((mod4Mask,                xK_F10           ), spawn "amixer -c 1 sset Master 2%+")
   ]
 
